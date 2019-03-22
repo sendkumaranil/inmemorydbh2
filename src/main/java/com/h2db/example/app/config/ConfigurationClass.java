f2accfce1e2aa5faba1bd6da39397c92f1e4a04f
anilkumar <anilkumar@192.168.0.104>
2019-03-22 23:59:23 +0530

    Profile based validation

--- src/main/java/com/h2db/example/app/config/ConfigurationClass.java
+++ src/main/java/com/h2db/example/app/config/ConfigurationClass.java
@@ -2,6 +2,7 @@ package com.h2db.example.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.validation.Validator;
 
 import com.h2db.example.app.validator.DataSourceValidator;
@@ -9,10 +10,10 @@ import com.h2db.example.app.validator.DataSourceValidator;
 @Configuration
 public class ConfigurationClass {
 
-
+    
 	@Bean
-	public static Validator configurationPropertiesValidator() {
-		return new DataSourceValidator();
+	public static Validator configurationPropertiesValidator(Environment env) {
+		return new DataSourceValidator(env);
 	}
 	
 }
